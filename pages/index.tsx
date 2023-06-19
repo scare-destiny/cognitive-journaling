65b6ef09fcde5fff648e3a45bb0dc07cd60e803f
scare-destiny <zhenya.venger@gmail.com>
2023-06-19 17:57:16 +0300

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,7 @@ const schemaArr = [
 export const ErrorMessage = ({ message }: { message: string }) => {
 	return (
 		<Box
-			sx={{
+			sx={{ 
 				p: 2,
 				rounded: 'md',
 				boxShadow: 'sm',
